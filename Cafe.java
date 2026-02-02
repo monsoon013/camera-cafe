cf9f14c4debdad4e0522d17504ae41c9f05f9051
monsoon013 <cael.corredera.perez@gmail.com>
2026-02-02 20:04:04 +0100

    second

--- Cafe.java
+++ Cafe.java
@@ -1,3 +1,17 @@
 public class Cafe {
     
+    private String nombre;
+    private int gCafe;
+    private int gCacao;
+    private int gLeche;
+
+    public Cafe (String nombre, int gCafe, int gCacao, int gLeche){
+        this.nombre = nombre;
+        this.gCafe = gCafe;
+        this.gCacao = gCacao;
+        this.gLeche = gLeche;
+    }
+
+    
+    
 }
--- Maquina.java
+++ Maquina.java
@@ -1,3 +1,26 @@
+import java.util.ArrayList;
+
 public class Maquina {
+
+    private String numeroSerie;
+    private ArrayList<Cafe> cafes;
+    private int vasos;
+    private int gCafe;
+    private int gLeche;
+    private int gCacao;
+    private int mlAgua;
+
+    public Maquina (String numeroSerie, ArrayList<Cafe> cafes, int vasos, 
+                    int gCafe, int gCacao, int gLeche, int mlAgua ){
+        this.numeroSerie = numeroSerie;
+        this.cafes = cafes;
+        this.vasos = vasos;
+        this.gCafe = gCafe;
+        this.gCacao = gCacao;
+        this.gLeche = gLeche;
+        this.mlAgua = mlAgua;
+    }
+
+    
     
 }
